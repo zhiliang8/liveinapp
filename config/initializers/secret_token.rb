ad0bb9d8a1ecb0c78461e6028c0ce33d818ad0ad
zhiliang <jordzhang@gmail.com>
2011-11-29 08:31:40 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liveinapp::Application.config.secret_token = '89fa0ce188c9412257119353a223a49754a4c7ec14c6af1f09f685866b1c051c5926f5a2a450b4069671f5d779f2c911fd2d1e6204a246d165fe8bb090f53a8e'
