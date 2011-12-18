378f1f8bf23235d4e83dda07a41393d05c95ea07
zhiliang <jordzhang@gmail.com>
2011-12-18 19:14:54 +0800

    modify

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,6 +2,7 @@
 class App < ActiveRecord::Base
   belongs_to :node, :counter_cache => true
   belongs_to :user
+  validates :name, :presence => true
   validates :url, :presence => true, :format => {:with => /http:\/\/\w+/}, :uniqueness => true
   mount_uploader :snapshot, SnapshotUploader
   
