f5d5f0a3649d9a77fd67f6e05ec433a5966aeadd
zhiliang <jordzhang@gmail.com>
2012-03-06 21:22:33 +0800

    optimize css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,9 @@ module ApplicationHelper
     @roots = Node.roots
     @childless = Node.childless
   end
+  
+  def timeago(time, options = {})
+    options[:class] ||= "timeago"
+    content_tag(:span, "<i class='icon-time'></i>#{t('common.created_at', :time => time_ago_in_words(time))}".html_safe, options.merge(:title => time.iso8601)) if time
+  end
 end
