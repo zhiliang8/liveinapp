2e6e0c283e1f97fd6abcfe6f8c5a6e56364ae00f
zhiliang <jordzhang@gmail.com>
2012-03-11 12:57:14 +0800

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class CommentsController < ApplicationController
   before_filter :authenticate_user!
   def create
@@ -5,9 +6,9 @@ class CommentsController < ApplicationController
     @comment = @app.comments.build(params[:comment])
     @comment.user_id = current_user.id
     if @comment.save
-      
+      flash.now[:notice] = "评论成功"
     else
-      
+      flash.now[:alert] = "评论失败"
     end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   
   attr_accessible :body
-  
   validates :body, :presence => true
+  
+  default_scope order("created_at desc")
 end
