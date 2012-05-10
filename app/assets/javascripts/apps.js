2bf8de8a317050d726fff6c1a5726d3d41bdbf23
zhiliang <jordzhang@gmail.com>
2012-05-10 21:38:05 +0800

    apps modify

--- app/assets/javascripts/apps.js
+++ app/assets/javascripts/apps.js
@@ -3,7 +3,7 @@
 $(function(){
 	$('#app-tab textarea').autogrow();
 	// $('#app-star').rating();
-	$("#latest-news h2 a").toggle(function() {
+	$("#latest-news h2 a[rel!=tooltip]").toggle(function() {
 	  $("#latest-news #news-form").show();
 	}, function(){
 	  $("#latest-news #news-form").hide();
